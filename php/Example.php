365d486602fd412a99746547ab790bc903917366
Matt (IPv4) <matthew@cowley.org.uk>
2018-01-23 09:36:44 +0000

    Create Example.php

---
+++ php/Example.php
@@ -0,0 +1,26 @@
+<?php
+
+// Note: This makes use of CURL and does not use a Discord API library so requires manual filling in of guild count and bot ID.
+
+$GUILDCOUNT = 10; // Change Me
+$BOTID = "DISCORDBOTID"; // Change Me
+$AUTH = "DBLAPIKEY"; // Change Me
+
+$dblData = ["server_count"=>$GUILDCOUNT];
+$dblData = json_encode($dblData);
+$dblURL = "https://discordbots.org/api/bots/".$BOTID."/stats";
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $dblURL);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $dblData);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+    'Authorization: '.$AUTH,
+    'Content-Type: application/json'
+));
+$result = curl_exec($ch);
+curl_close($ch);
+
+?>
