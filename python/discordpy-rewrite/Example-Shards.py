a379186350a68b8e4a0489f8a5ad48476ce3c20d
Matt (IPv4) <matthew@cowley.org.uk>
2018-01-28 09:19:07 +0000

    Update Example-Shards.py

--- python/discordpy-rewrite/Example-Shards.py
+++ python/discordpy-rewrite/Example-Shards.py
@@ -6,6 +6,7 @@ class DiscordBotsOrgAPI:
     def __init__(self, bot):
         self.bot = bot
         self.session = aiohttp.ClientSession(loop=self.bot.loop)
+        self.key = 'DBLAPIKEY'
         
     def __unload(self):
         self.bot.loop.create_task(self.session.close())
@@ -17,19 +18,19 @@ class DiscordBotsOrgAPI:
             'server_count': len(self.bot.guilds)
         })
         head = {
-            'Authorization': 'DBLAPIKEY',
+            'Authorization': self.key,
             'Content-type' : 'application/json'
         }
 
         url = '{}/bots/{}/stats'.format('https://discordbots.org/api', self.bot.user.id)
 
-        async with self.session.post(url, data=payload, headers=head) as resp:
-        if req.status == 200:
-            logging.info('poster[dbl]: done')
-        else:
-            t = await req.text()
-            logging.error('poster[dbl]: failed (code {})'.format(req.status))
-            logging.error('poster[dbl]: response: {}'.format(t))
+        async with self.session.post(url, data=payload, headers=head) as req:
+            if req.status == 200:
+                logging.info('poster[dbl]: done')
+            else:
+                t = await req.text()
+                logging.error('poster[dbl]: failed (code {})'.format(req.status))
+                logging.error('poster[dbl]: response: {}'.format(t))
         
     async def on_guild_join(self, guild):
         await self.update()
