443606da735654abe4c13867b4557b7b033cbcbe
Matt (IPv4) <matthew@cowley.org.uk>
2018-01-28 09:10:25 +0000

    Update Example-NoShards.py

--- python/discordpy-rewrite/Example-NoShards.py
+++ python/discordpy-rewrite/Example-NoShards.py
@@ -10,7 +10,7 @@ class DiscordBotsOrgAPI:
     def __unload(self):
         self.bot.loop.create_task(self.session.close())
         
-    async def send(self):
+    async def update(self):
         payload = json.dumps({
             'server_count': len(self.bot.guilds)
         })
