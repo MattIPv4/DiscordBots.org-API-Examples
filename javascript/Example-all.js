bffcdd93a42f6bb8338296f364df4b4c6fa4cacb
Frangu Vlad <contact@kingdgrizzle.ml>
2018-03-29 21:29:40 +0300

    Add more documentations

--- javascript/Example-all.js
+++ javascript/Example-all.js
@@ -6,8 +6,14 @@ const dblposter = require("dblposter");
 
 /**
  * `client` must be your client instance
+ * After this, you are able to access this instance anywhere with <client>.dblPoster
+ * or whatever value you chose when binding the poster to your client.
  */
 const dbl = new dblposter(client, "DBLAPIKEY");
 
 // Binds the client to the internal timer
+// You can also pass in a custom name for the value that will get bound to the client.
 dbl.bind();
+
+// If you want to manually trigger a statistics post, for some reason.
+dbl.post();
