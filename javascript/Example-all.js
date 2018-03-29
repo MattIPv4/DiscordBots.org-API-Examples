78179fb897b8865621cad5abc08cc4f42492d2b1
Frangu Vlad <contact@kingdgrizzle.ml>
2018-03-29 11:42:24 +0300

    Make DBLAPIKEY be a string in the example code

--- javascript/Example-all.js
+++ javascript/Example-all.js
@@ -7,7 +7,7 @@ const dblposter = require("dblposter");
 /**
  * `client` must be your client instance
  */
-const dbl = new dblposter(client, DBLAPIKEY);
+const dbl = new dblposter(client, "DBLAPIKEY");
 
 // Binds the client to the internal timer
 dbl.bind();
